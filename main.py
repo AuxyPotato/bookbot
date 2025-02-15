0325e58c5b9a728661b271e2e3b0e5b7499b7e77
AuxyPotato <lucyschonewille@gmail.com>
2025-02-15 22:33:39 +0100

    WHOOOO YEAH BABY THATS WHAT IM TALKING ABOUT

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_text(path_to_file)
     num_words = wordcount(file_contents)
-    num_chars = charcount(file_contents)
+    char_list = charcount(file_contents)
+    sorted_chars = sorted(
+        [(char, count) for char, count in char_list if char.isalpha()], 
+        key=lambda pair: pair[1],
+        reverse=True)
     print(f"--- Begin report of {path_to_file} ---")
     print(f"{num_words} words found in the document")
-    print(num_chars)
+    print("")
+    for char, count in sorted_chars:
+        print(f"The '{char}' character was found {count} times")
 
 
 def wordcount(file_contents):
     return(len(file_contents.split()))
 
 def charcount(file_contents):
-    return(Counter(file_contents.lower()))
+    return(list(Counter(file_contents.lower()).items()))
 
 def get_text(path):
     with open(path) as f:
