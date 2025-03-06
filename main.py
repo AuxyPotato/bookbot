c4c0b78eb68a3b7f2b1665250f5f0c0b224193c0
AuxyPotato <lucyschonewille@gmail.com>
2025-03-06 21:14:57 +0100

    separated functions

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # bookbot 
 # boot.dev guided project
+# a small program that calculates the amount of characters and words in a book
 # author: lucy
 
-from stats import *
+from stats import wordcount, charcount, sort_characters
 
 def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_text(path_to_file)
     num_words = wordcount(file_contents)
     char_list = charcount(file_contents)
-    sorted_chars = sorted(
-        [(char, count) for char, count in char_list if char.isalpha()], 
-        key=lambda pair: pair[1],
-        reverse=True)
+    sorted_chars = sort_characters(char_list)
+    generate_report(path_to_file, num_words, sorted_chars)
 
+def get_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def generate_report(path_to_file, num_words, sorted_chars):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_file}...")
     print("----------- Word Count ----------")
@@ -23,12 +27,6 @@ def main():
     for char, count in sorted_chars:
         print(f"The '{char}' character was found {count} times")
     print("============= END ===============")
-
-def get_text(filepath):
-    with open(filepath) as f:
-        return f.read()
-
-
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 # bookbot
+# boot.dev guided project
 # statistics calculation
 # author: lucy
 
@@ -10,4 +11,10 @@ def wordcount(file_contents):
 
 def charcount(file_contents):
     return(list(Counter(file_contents.lower()).items()))
-    
+
+def sort_characters(char_list):
+    sorted_chars = sorted(
+        [(char, count) for char, count in char_list if char.isalpha()], 
+        key=lambda pair: pair[1],
+        reverse=True)
+    return sorted_chars
